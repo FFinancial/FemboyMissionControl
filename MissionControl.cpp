f0a35b6c615b93ee290fad17eb7ecc579e8d8bd2
James Shiffer <basedvioletfag@waifu.club>
2020-12-06 11:07:02 -0800

    oopsie

--- MissionControl.cpp
+++ MissionControl.cpp
@@ -143,6 +143,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         {
             klaxon.play();
             klaxon.setLoop(true);
+            timer.restart();
         }
         else
         {
